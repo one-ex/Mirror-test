52e531edbbc5f8e9e644d1625a4e389b86824488
one-ex <exball649@gmail.com>
2026-02-06 03:28:58 +0800

    Mirror-test/render_pixeldrain_mirror.py

--- render_pixeldrain_mirror.py
+++ render_pixeldrain_mirror.py
@@ -240,7 +240,7 @@ def index():
     })
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     
     # Check for API key
     if not mirror_service.api_key:
